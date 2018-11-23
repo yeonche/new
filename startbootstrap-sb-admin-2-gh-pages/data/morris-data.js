0ee4ff6334ecf40f52d36d5dd9045768f6b02fee
yeonche <ssmokie10@naver.com>
2018-11-23 19:39:58 +0900

    채팅보강

--- startbootstrap-sb-admin-2-gh-pages/data/morris-data.js
+++ startbootstrap-sb-admin-2-gh-pages/data/morris-data.js
@@ -1,115 +1,135 @@
 $(function() {
 
-    Morris.Area({
-        element: 'morris-area-chart',
-        data: [{
-            period: '2010 Q1',
-            iphone: 2666,
-            ipad: null,
-            itouch: 2647
-        }, {
-            period: '2010 Q2',
-            iphone: 2778,
-            ipad: 2294,
-            itouch: 2441
-        }, {
-            period: '2010 Q3',
-            iphone: 4912,
-            ipad: 1969,
-            itouch: 2501
-        }, {
-            period: '2010 Q4',
-            iphone: 3767,
-            ipad: 3597,
-            itouch: 5689
-        }, {
-            period: '2011 Q1',
-            iphone: 6810,
-            ipad: 1914,
-            itouch: 2293
-        }, {
-            period: '2011 Q2',
-            iphone: 5670,
-            ipad: 4293,
-            itouch: 1881
-        }, {
-            period: '2011 Q3',
-            iphone: 4820,
-            ipad: 3795,
-            itouch: 1588
-        }, {
-            period: '2011 Q4',
-            iphone: 15073,
-            ipad: 5967,
-            itouch: 5175
-        }, {
-            period: '2012 Q1',
-            iphone: 10687,
-            ipad: 4460,
-            itouch: 2028
-        }, {
-            period: '2012 Q2',
-            iphone: 8432,
-            ipad: 5713,
-            itouch: 1791
-        }],
-        xkey: 'period',
-        ykeys: ['iphone', 'ipad', 'itouch'],
-        labels: ['iPhone', 'iPad', 'iPod Touch'],
-        pointSize: 2,
-        hideHover: 'auto',
-        resize: true
-    });
+    // Morris.Area({
+    //     element: 'morris-area-chart',
+    //     data: [{
+    //         period: '2010 Q1',
+    //         iphone: 2666,
+    //         ipad: 3308,
+    //         itouch: 2647,
+    //         macbook:1092
+    //     }, {
+    //         period: '2010 Q2',
+    //         iphone: 2778,
+    //         ipad: 2294,
+    //         itouch: 2441,
+    //         macbook:1092
+    //     }, {
+    //         period: '2010 Q3',
+    //         iphone: 4912,
+    //         ipad: 1969,
+    //         itouch: 2501,
+    //         macbook:1092
+    //     }, {
+    //         period: '2010 Q4',
+    //         iphone: 3767,
+    //         ipad: 3597,
+    //         itouch: 5689,
+    //         macbook:1092
+    //     }, {
+    //         period: '2011 Q1',
+    //         iphone: 6810,
+    //         ipad: 1914,
+    //         itouch: 2293,
+    //         macbook:1092
+    //     }, {
+    //         period: '2011 Q2',
+    //         iphone: 5670,
+    //         ipad: 4293,
+    //         itouch: 1881,
+    //         macbook: 1121
+    //     }, {
+    //         period: '2011 Q3',
+    //         iphone: 4820,
+    //         ipad: 3795,
+    //         itouch: 1588,
+    //         macbook: 1172
+    //     }, {
+    //         period: '2011 Q4',
+    //         iphone: 15073,
+    //         ipad: 5967,
+    //         itouch: 5175,
+    //         macbook: 1701
+    //     }, {
+    //         period: '2012 Q1',
+    //         iphone: 10687,
+    //         ipad: 000,
+    //         itouch: 2028,
+    //         zacbook : 1875
+    //     }, {
+    //         period: '2012 Q2',
+    //         iphone: 8432,
+    //         ipad: 5713,
+    //         itouch: 1791,
+    //         macbook: 2000
+    //     }],
+    //     xkey: 'period',
+    //     ykeys: ['iphone', 'ipad', 'itouch'],
+    //     labels: ['iPhone', 'iPad', 'iPod Touch'],
+    //     pointSize: 2,
+    //     hideHover: 'auto',
+    //     resize: true
+    // });
 
-    Morris.Donut({
-        element: 'morris-donut-chart',
-        data: [{
-            label: "Download Sales",
-            value: 12
-        }, {
-            label: "In-Store Sales",
-            value: 30
-        }, {
-            label: "Mail-Order Sales",
-            value: 20
-        }],
-        resize: true
-    });
+    // Morris.Donut({
+        // element: 'morris-donut-chart',
+        // data: [{
+            // label: "Download Sales",
+            // value: 25
+    //     // }, {
+    //         label: "In-Store Sales",
+    //         value: 25
+    //     }, {
+    //         label: "Mail-Order Sales",
+    //         value: 25
+    //     }, {
+    //         label: "Mail-Order Sales",
+    //         value: 25
+    //     }],
+    //     resize: true
+    // });
 
     Morris.Bar({
         element: 'morris-bar-chart',
         data: [{
             y: '2006',
             a: 100,
-            b: 90
+            b: 90,
+            c: 10
         }, {
             y: '2007',
             a: 75,
-            b: 65
+            b: 65,
+            c: 90
         }, {
             y: '2008',
             a: 50,
-            b: 40
+            b: 40,
+            c: 30
         }, {
             y: '2009',
             a: 75,
-            b: 65
+            b: 65,
+            c: 60
         }, {
             y: '2010',
             a: 50,
-            b: 40
+            b: 40,
+            c: 65
         }, {
             y: '2011',
             a: 75,
-            b: 65
+            b: 65,
+            c: 70
         }, {
             y: '2012',
             a: 100,
-            b: 90
+            b: 90,
+            c: 120
         }],
-        xkey: 'y',
-        ykeys: ['a', 'b'],
-        labels: ['Series A', 'Series B'],
+        xkey: 'period',
+        ykeys: ['a', 'b', 'c'],
+        labels: ['Series A', 'Series B', 'Series C'],
         hideHover: 'auto',
         resize: true
     });
